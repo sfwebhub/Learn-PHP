053c27d5e14a56ea29cfa7283c926e234f6a3a92
Faisal Ahmed <info.sfwebhub@gmail.com>
2025-04-28 11:12:39 +0600

    Lean Syntax

---
+++ Syntax.php
@@ -0,0 +1,13 @@
+ <?php
+
+ /*echo "Hello World";
+  Echo "Hello World";
+  ECHO "Hello World"?  */
+
+ 
+  $color = "red";
+  echo "My Shirt is " . $color .  "<br>";
+  echo "My Bag is " . $COLOR . "<br>";
+  echo "My Pen is " . $coLOR .  "<br>";
+ 
+ ?>
