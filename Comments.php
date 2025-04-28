119ae4404427492897e64a9b049739ce24c67147
Faisal Ahmed <info.sfwebhub@gmail.com>
2025-04-28 11:20:50 +0600

    Learn Comments

---
+++ Comments.php
@@ -0,0 +1,19 @@
+// It is for others understand the code.
+Remind yourself of what you did.
+
+Single line comment like
+
+// or #   
+
+
+//Hello World   or
+
+* Hello World
+
+Multi Line Comments Like 
+
+
+/*   Hello World
+     Hello world */
+   
+
