36b45b5f7d840c69a9def1e4affa07e826f54a4d
Faisal Ahmed <info.sfwebhub@gmail.com>
2025-04-29 12:36:01 +0600

    Learn PHP Data Types

---
+++ PHP Data Types/Integer.php
@@ -0,0 +1,7 @@
+
+//An integer data type is a non-decimal number between -2,147,483,648 and 2,147,483,647.
+<?php
+
+$x= 5985;
+
+var_dump($x);
---
+++ PHP Data Types/String.php
@@ -0,0 +1,27 @@
+/* PHP Data Types
+Variables can store data of different types, and different data types can do different things.
+
+PHP supports the following data types:
+
+String
+Integer
+Float (floating point numbers - also called double)
+Boolean
+Array
+Object
+NULL
+Resource */
+
+//String //
+
+
+<?php
+
+$x ="Hello World";
+$y = "Hello Bangladesh";
+
+var_dump ($x);
+echo "<br>";
+var_dump($y);
+
+
