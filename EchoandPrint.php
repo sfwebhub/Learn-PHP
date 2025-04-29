4f17b905e52d7fed06f8f35a87d750eb195d8e5b
Faisal Ahmed <info.sfwebhub@gmail.com>
2025-04-29 11:49:26 +0600

    Learn Echo and Print

---
+++ EchoandPrint.php
@@ -0,0 +1,25 @@
+<?php
+//Display Text
+/*echo "Hello";
+//same as
+echo ("Hello World"); */
+
+echo "<h1>PHP is a Fun <h1>";
+echo "Hello World <br>";
+echo "I'm about to learn PHP <br>";
+echo "This",  "String",  "Was", "Made", "with multiple parameters.";
+
+
+//Display Variables
+
+$text1= "Lean PHP";
+$text2= "Faisal's School";
+
+echo "<h2>I $text1<h2>";
+echo "<p>Study at $text2<p>";
+
+
+
+//Using Single Quotes
+print"<h2>" .$text1 . "<h2";
+print "<p> Study at" .$text2. "<p>";
