34d2c5d4d867f7ad9ee7c8d136e81b6ad232e3a9
Faisal Ahmed <info.sfwebhub@gmail.com>
2025-04-29 11:09:33 +0600

    Learn Variable

---
+++ Variables/Variables.php
@@ -0,0 +1,30 @@
+/* A variable can have a short name (like $x and $y) or a more descriptive name ($age, $name, $total_volume). */
+
+
+<?php
+
+ /* $txt = "Farhan" ;
+
+echo "My name is  "  .$txt ; */
+
+/* $x=5;
+$y=5;
+
+echo $x+$y; */
+
+/* $x= "Fahan";
+$y= 10;
+$z= 3.15;
+
+var_dump ($x);
+var_dump($y);
+var_dump($z);  */
+
+var_dump("Faria");
+var_dump(10);
+var_dump(3.20);
+var_dump(true);
+var_dump ([2,5,50]);
+var_dump(Null);
+
+
