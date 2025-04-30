288c2ff0755e315679014887857186c6ebe906ef
Faisal Ahmed <info.sfwebhub@gmail.com>
2025-04-30 22:19:04 +0600

    Learn Casting

---
+++ Casting.php
@@ -0,0 +1,110 @@
+<?php
+
+/*Cast to String
+
+$a = 5;       // Integer
+$b = 5.34;    // Float
+$c = "hello"; // String
+$d = true;    // Boolean
+$e = NULL;    // NULL
+
+$a = (string) $a;
+$b = (string) $b;
+$c = (string) $c;
+$d = (string) $d;
+$e = (string) $e;
+
+//To verify the type of any object in PHP, use the var_dump() function:
+var_dump($a);
+var_dump($b);
+var_dump($c);
+var_dump($d);
+var_dump($e);  */
+
+
+/*Cast to Integer
+$a = 5;       // Integer
+$b = 5.34;    // Float
+$c = "25 kilometers"; // String
+$d = "kilometers 25"; // String
+$e = "hello"; // String
+$f = true;    // Boolean
+$g = NULL;    // NULL
+
+$a = (int) $a;
+$b = (int) $b;
+$c = (int) $c;
+$d = (int) $d;
+$e = (int) $e;
+$f = (int) $f;
+$g = (int) $g;  */
+
+/* Cast to Float
+
+$a = 5;       // Integer
+$b = 5.34;    // Float
+$c = 0;       // Integer
+$d = -1;      // Integer
+$e = 0.1;     // Float
+$f = "hello"; // String
+$g = "";      // String
+$h = true;    // Boolean
+$i = NULL;    // NULL
+
+$a = (bool) $a;
+$b = (bool) $b;
+$c = (bool) $c;
+$d = (bool) $d;
+$e = (bool) $e;
+$f = (bool) $f;
+$g = (bool) $g;
+$h = (bool) $h;
+$i = (bool) $i; */
+
+/*Cast to Array
+
+$a = 5;       // Integer
+$b = 5.34;    // Float
+$c = "hello"; // String
+$d = true;    // Boolean
+$e = NULL;    // NULL
+
+$a = (array) $a;
+$b = (array) $b;
+$c = (array) $c;
+$d = (array) $d;
+$e = (array) $e; */
+
+/*
+Cast to Object
+
+
+
+$a = 5;       // Integer
+$b = 5.34;    // Float
+$c = "hello"; // String
+$d = true;    // Boolean
+$e = NULL;    // NULL
+
+$a = (object) $a;
+$b = (object) $b;
+$c = (object) $c;
+$d = (object) $d;
+$e = (object) $e; */
+
+/*
+Cast to NULL
+
+$a = 5;       // Integer
+$b = 5.34;    // Float
+$c = "hello"; // String
+$d = true;    // Boolean
+$e = NULL;    // NULL
+
+$a = (unset) $a;
+$b = (unset) $b;
+$c = (unset) $c;
+$d = (unset) $d;
+$e = (unset) $e;  */
+
+
