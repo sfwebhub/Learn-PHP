9dd491af2598c892a42a931aa86ce0c528e19940
Faisal Ahmed <info.sfwebhub@gmail.com>
2025-04-30 23:08:15 +0600

    Learn Math

---
+++ Math.php
@@ -0,0 +1,23 @@
+<?php
+
+//PHP pi() Function
+echo(pi());
+
+//PHP min() and max() Functions
+
+echo(min(0, 150, 30, 20, -8, -200));
+echo(max(0, 150, 30, 20, -8, -200));
+
+//PHP abs() Function
+
+echo(sqrt(64));
+
+//PHP round() Function
+
+echo(round(0.60));
+echo(round(0.49));
+
+//Random Numbers
+echo(rand());
+
+echo(rand(10, 100));
